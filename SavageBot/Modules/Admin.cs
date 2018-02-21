509e9cfe2d58319deba846d42074427aff45d225
CodingDab <nickallbritton8664@gmail.com>
2018-02-21 09:33:53 -0600

    kick command

--- SavageBot/Modules/Admin.cs
+++ SavageBot/Modules/Admin.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Discord.Commands;
+using Discord.WebSocket;
 
 namespace SavageBot.Modules
 {
     [Group("admin")]
-    [RequireUserPermissionAttribute(Discord.GuildPermission.Administrator)]
     public class Admin : ModuleBase<SocketCommandContext>
     {
-
+        public class Kick : ModuleBase<SocketCommandContext>
+        {
+            [Command("kick")]
+            [RequireUserPermission(Discord.GuildPermission.KickMembers)]
+            public async Task KickAsync(SocketGuildUser user, string reason = null)
+            {
+                await user.KickAsync(reason ?? null);
+            }
+        }
     }
 }
