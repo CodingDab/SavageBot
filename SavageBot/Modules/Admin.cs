6b2a138045266c2d63ee2d2c144de20be1305825
CodingDab <nickallbritton8664@gmail.com>
2018-02-21 10:15:16 -0600

    added ban and unban commands

--- SavageBot/Modules/Admin.cs
+++ SavageBot/Modules/Admin.cs
@@ -20,5 +20,34 @@ namespace SavageBot.Modules
                 await user.KickAsync(reason ?? null);
             }
         }
+
+        public class Ban : ModuleBase<SocketCommandContext>
+        {
+            [Command("ban")]
+            [RequireUserPermission(Discord.GuildPermission.BanMembers)]
+            public async Task BanAsync(SocketGuildUser user, string reason = null)
+            {
+                await Context.Guild.AddBanAsync(user, 0, reason);
+            }
+        }
+
+        public class Unban : ModuleBase<SocketCommandContext>
+        {
+            [Command("unban")]
+            [RequireUserPermission(Discord.GuildPermission.BanMembers)]
+            public async Task UnBanAsync(ulong id)
+            {
+                foreach(var x in Context.Client.Guilds)
+                {
+                    foreach( var y in x.Users)
+                    {
+                        if(y.Id == id)
+                        {
+                            await Context.Guild.RemoveBanAsync(y);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
