a4814eb815a13383e3b5d7f9e8adf4ac0fe7e29d
CodingDab <nickallbritton8664@gmail.com>
2018-02-23 09:20:44 -0600

    added a function to easily find a Guild's stored data given its id

--- SavageBot/GuildCenter.cs
+++ SavageBot/GuildCenter.cs
@@ -9,5 +9,13 @@ namespace SavageBot
     class GuildCenter
     {
         public static List<GuildData> Guilds = new List<GuildData>();
+        public static GuildData GetGuild(ulong id)
+        {
+            foreach(var x in Guilds)
+            {
+                if (x.Id == id) return x;
+            }
+            return new GuildData(0); // 0 id means that the guild was not found
+        }
     }
 }
