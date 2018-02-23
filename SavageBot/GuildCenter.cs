e12c0a8634c7e89a0c7ccbdcaab3264741bcb613
CodingDab <nickallbritton8664@gmail.com>
2018-02-23 08:52:21 -0600

    added a class GuildData to store guild data and GuildCenter to store all guilds
    GuildData stores all info that will need to be accessed later on. GuildCenter holds a List<> of all GuildData's that have been created in the programs lifetime.

---
+++ SavageBot/GuildCenter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SavageBot
+{
+    class GuildCenter
+    {
+        public static List<GuildData> Guilds = new List<GuildData>();
+    }
+}
---
+++ SavageBot/GuildData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace SavageBot
+{
+    class GuildData
+    {
+        public ulong Id;
+        public SocketChannel defaultChannel;
+        public GuildData(ulong Id)
+        {
+            this.Id = Id;
+        }
+    }
+}
